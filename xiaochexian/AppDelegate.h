9945ddb116557159a48a46d197f4e3b323328776
逯玲 <309604965@qq.com>
2016-03-17 17:20:30 +0800

    Initial Commit

---
+++ xiaochexian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xiaochexian
+//
+//  Created by 逯玲 on 16/3/17.
+//  Copyright © 2016年 蒯全伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xiaochexian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xiaochexian
+//
+//  Created by 逯玲 on 16/3/17.
+//  Copyright © 2016年 蒯全伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
