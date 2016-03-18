16dacbf3d47020565e4f70da8187e48772f4d51d
duchen <duchen@staff.sina.com.cn>
2016-03-18 18:43:12 +0800

    测试ssid获取

--- Today/TodayViewController.m
+++ Today/TodayViewController.m
@@ -8,23 +8,18 @@
 
 #import "TodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
+#import <SystemConfiguration/CaptiveNetwork.h>
+#import <NetworkExtension/NetworkExtension.h>
 
 @interface TodayViewController () <NCWidgetProviding>
 
 @end
 
 @implementation TodayViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-    
+{
+    UILabel *_layout;
+    NSTimer *_timer;
+    NSInteger _count;
 }
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
@@ -33,8 +28,83 @@
     // If an error is encountered, use NCUpdateResultFailed
     // If there's no update required, use NCUpdateResultNoData
     // If there's an update, use NCUpdateResultNewData
-
+    
     completionHandler(NCUpdateResultNewData);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    for (UIView *v in self.view.subviews) {
+        if ([v isKindOfClass:[UILabel class]]) {
+            _layout = (UILabel *)v;
+        }
+    }
+    
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1
+                                              target:self
+                                            selector:@selector(loop)
+                                            userInfo:nil
+                                             repeats:YES];
+}
+
+#pragma mark - Test
+
+- (void)loop
+{
+    id data = [self fetchSSIDInfoOld];
+    if ([data isKindOfClass:[NSDictionary class]]) {
+        NSDictionary *dic = (NSDictionary *)data;
+        NSString *mac = [dic objectForKey:@"BSSID"];
+        NSString *name = [dic objectForKey:@"SSID"];
+        
+        _layout.text = [NSString stringWithFormat:@"Counting : %zi\nBSSID : %@\nSSID : %@", _count++, mac, name];
+    }
+}
+
+- (id)fetchSSIDInfo
+{
+    NSArray *networkInterfaces = [NEHotspotHelper supportedNetworkInterfaces];
+    NSLog(@"Networks %@",networkInterfaces);
+    
+    for (NEHotspotNetwork *hotspotNetwork in networkInterfaces) {
+        NSString *ssid = hotspotNetwork.SSID;
+        NSString *bssid = hotspotNetwork.BSSID;
+        BOOL secure = hotspotNetwork.secure;
+        BOOL autoJoined = hotspotNetwork.autoJoined;
+        double signalStrength = hotspotNetwork.signalStrength;
+        NSLog(@"ssid = %");
+    }
+    return nil;
+}
+
+- (id)fetchSSIDInfoOld
+{
+    NSString *currentSSID = @"";
+    CFArrayRef myArray = CNCopySupportedInterfaces();
+    if (myArray != nil) {
+        NSDictionary* myDict = (__bridge NSDictionary *) CNCopyCurrentNetworkInfo(CFArrayGetValueAtIndex(myArray, 0));
+        if (myDict != nil) {
+            currentSSID = [myDict valueForKey:@"SSID"];
+        }
+        else {
+            currentSSID = @"<<NONE>>";
+        }
+    }
+    else {
+        currentSSID = @"<<NONE>>";
+    }
+    
+    NSArray *ifs = (__bridge id)CNCopySupportedInterfaces();
+    NSLog(@"%s: Supported interfaces: %@", __func__, ifs);
+    id info = nil;
+    for (NSString *ifnam in ifs) {
+        info = (__bridge id)CNCopyCurrentNetworkInfo((CFStringRef)CFBridgingRetain(ifnam));
+        if (info && [info count]) {
+            break;
+        }
+    }
+    return info;
 }
 
 @end
